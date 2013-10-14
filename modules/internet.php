d99cf3edb641fba836383efab00feb8d3255e58c
Kunal Gautam <kunal@ikunal.in>
2013-10-15 03:43:38 +0530

    Discarded idea of checking internet connectivity via ping as ICMP might be blocked by some ISP

--- modules/internet.php
+++ modules/internet.php
@@ -1,5 +1,8 @@
 <?php
-echo shell_exec('
-((ping -w5 -c3 8.8.8.8 || ping -w5 -c3 4.2.2.1) > /dev/null 2>&1) && echo "Connected" || (echo "Disconnected" && exit 1)
-');
+$fp = fsockopen("www.google.com", 80, $errno, $errstr, 30);
+if (!$fp) {
+    echo "Disconnected";
+} else {
+    echo "Connected";
+}
 ?>
