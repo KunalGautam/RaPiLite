c0573af8fa6ba6f5d244bd64c597d9d38f81307f
Kunal Gautam <kunal@ikunal.in>
2013-10-16 16:01:02 +0530

    Integrated module to main file.

--- index.php
+++ index.php
@@ -6,9 +6,86 @@ include 'header.php';
       <!-- Begin page content -->
       <div class="container">
         <div class="page-header">
-          <h1>Status Report of <?php echo shell_exec('hostname'); ?></h1>
+          <h1>Status Report of Your System</h1>
         </div>
-       			Some Content
+<table class="table">
+        <thead>
+          <tr>
+            <th>Service/Item</th>
+            <th>Status</th>
+                     </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Hostname</td>
+            <td><?php include 'modules/hostname.php'; ?></td>
+           
+          </tr>
+
+
+
+<tr>
+            <td>Network Details for eth0</td>
+            <td><?php include 'modules/network.php'; ?></td>
+
+          </tr>
+
+
+<tr>
+            <td>Nameserver Assigned</td>
+            <td><?php include 'modules/nameserver.php'; ?></td>
+
+          </tr>
+
+
+<tr>
+            <td>Uptime</td>
+            <td><?php include 'modules/uptime.php'; ?></td>
+
+          </tr>
+
+<tr>
+            <td>System Time</td>
+            <td><?php include 'modules/time.php'; ?></td>
+
+          </tr>
+
+<tr>
+            <td>System Temprature</td>
+            <td><?php include 'modules/temprature.php'; ?>&deg;C</td>
+
+          </tr>
+
+
+<tr>
+            <td>Internet Status</td>
+            <td><?php include 'modules/internet.php'; ?></td>
+
+          </tr>
+
+
+<tr>
+            <td>Disk Status</td>
+            <td><?php include 'modules/diskspace.php'; ?></td>
+
+          </tr>
+
+<tr>
+            <td>System Load</td>
+            <td><?php include 'modules/load.php'; ?></td>
+
+          </tr>
+
+<tr>
+            <td>Hostname</td>
+            <td><?php include 'modules/hostname.php'; ?></td>
+
+          </tr>
+
+
+        </tbody>
+      </table>
+       	
       </div>
     </div>
 
